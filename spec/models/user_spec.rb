54dfad88a161169048a601ba73093c0bc4bc0b7a
Steven Wu <wusteven@gmail.com>
2012-09-17 23:37:31 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,38 +12,39 @@
 require 'spec_helper'
 
 describe User do
-	before { @user = User.new(name: "Example User",
+  before { @user = User.new(name: "Example User",
           email: "user@example.com",
           password: "foobar",
           password_confirmation: "foobar") }
 
-	subject { @user }
+  subject { @user }
 
-	it { should respond_to(:name) }
-	it { should respond_to(:email) }
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
-	it { should be_valid }
+  it { should be_valid }
 
-	describe "when name is not present" do
-		before { @user.name = "" }
-		it { should_not be_valid }
-	end
+  describe "when name is not present" do
+    before { @user.name = "" }
+    it { should_not be_valid }
+  end
 
-	describe "when email is not present" do
-		before { @user.email = "" }
-		it { should_not be_valid }
-	end
+  describe "when email is not present" do
+    before { @user.email = "" }
+    it { should_not be_valid }
+  end
 
-	describe "when name is too long" do
-		before { @user.name = "a" * 51 }
-		it { should_not be_valid }
-	end
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
 
-	describe "when email format is invalid" do
+  describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -65,13 +66,13 @@ describe User do
   end
 
   describe "when email address is already taken" do
-  	before do
-  		user_with_same_email = @user.dup
-  		user_with_same_email.email = @user.email.upcase
-  		user_with_same_email.save
-  	end
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
 
-  	it { should_not be_valid }
+    it { should_not be_valid }
   end
 
   describe "when password is not present" do
@@ -118,6 +119,11 @@ describe User do
       @user.save
       @user.reload.email.should == mixed_case_email.downcase
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
 
